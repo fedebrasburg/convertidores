c120e87c4b2108b1b6a43b7c6b51b4ad7ff098df
fedebrasburg <federico.brasburg@gmail.com>
2021-09-07 23:11:26 -0300

    Catch de errores del convertidor compras

--- salidaCompras/convertidorCompras.py
+++ salidaCompras/convertidorCompras.py
@@ -27,53 +27,50 @@ import re
 # 8: Perc. iva
 # 9: Total
 
-def procesarLinea(linea):
-    index = 1
-    while(index > 0):
-      if (cuitRegex.search(linea[index])):
-        linea[1:index] = [' '.join(linea[1:index])]
-        break
-      index  += 1
-    return linea
+class LineaConFormatoIncorrecto(Exception):
+  pass
 
 cuitRegex = re.compile(r'\d-\d\d\d\d\d\d\d\d-\d')
 def main():
     salidas = []
     with open('Compras.inf') as f:
-        for linea in f:
-            # linea = procesarlinea(linea.split())
-            linea = linea.split()
+        for l in f:
+            linea = l.split()
             index = 1
-            while(index > 0):
-                if (cuitRegex.search(linea[index])):
-                    linea[1:index] = [' '.join(linea[1:index])]
-                    break
-                index  += 1
-            fecha         = linea[0]
-            nombre        = linea[1]
-            cuit          = linea[2]
-            numeroFactura = linea[3]
-            exento        = linea[4]
-            gravado       = linea[5]
-            iva           = linea[6]
-            percIB        = linea[7]
-            perIva        = linea[8]
-            total         = linea[9]
-            salidas.append([
-                '/'.join(fecha[i:i+2] for i in range(0, len(fecha), 2)),
-                nombre,
-                "RI",
-                cuit,
-                "NCC" if float(total.replace(",", ".")) < 0 else "FCC",
-                "A" + str(numeroFactura),
-                exento,
-                gravado,
-                iva,
-                percIB,
-                "0,00",
-                perIva,
-                total
-            ])
+            try:
+              while(index > 0):
+                  if (cuitRegex.search(linea[index])):
+                      linea[1:index] = [' '.join(linea[1:index])]
+                      break
+                  index  += 1
+              fecha         = linea[0]
+              nombre        = linea[1]
+              cuit          = linea[2]
+              numeroFactura = linea[3]
+              exento        = linea[4]
+              gravado       = linea[5]
+              iva           = linea[6]
+              percIB        = linea[7]
+              perIva        = linea[8]
+              total         = linea[9]
+              salidas.append([
+                  '/'.join(fecha[i:i+2] for i in range(0, len(fecha), 2)),
+                  nombre,
+                  "RI",
+                  cuit,
+                  "NCC" if float(total.replace(",", ".")) < 0 else "FCC",
+                  "A" + str(numeroFactura),
+                  exento,
+                  gravado,
+                  iva,
+                  percIB,
+                  "0,00",
+                  perIva,
+                  total
+              ])
+            except:
+              print("Falle al procesar la linea: " + l)
+              raise LineaConFormatoIncorrecto
 
     with open('salidaCompras.txt', 'w') as file:
         for linea in salidas:
